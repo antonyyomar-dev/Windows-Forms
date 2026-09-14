b4d6abd2ff8f6bbf018db70a8284ca189294b5d0
Antony Yomar Peña Roña <antonyyomar202403@gmail.com>
2026-09-13 23:55:37 -0500

    Add files via upload

---
+++ Lab01/Lab01/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Lab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviarMensaje = new System.Windows.Forms.Button();
+            this.lblForm = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEnviarMensaje
+            // 
+            this.btnEnviarMensaje.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEnviarMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarMensaje.Location = new System.Drawing.Point(295, 166);
+            this.btnEnviarMensaje.Name = "btnEnviarMensaje";
+            this.btnEnviarMensaje.Size = new System.Drawing.Size(177, 58);
+            this.btnEnviarMensaje.TabIndex = 0;
+            this.btnEnviarMensaje.Text = "Enviar Mensaje";
+            this.btnEnviarMensaje.UseVisualStyleBackColor = false;
+            this.btnEnviarMensaje.Click += new System.EventHandler(this.btnEnviarMensaje_Click);
+            // 
+            // lblForm
+            // 
+            this.lblForm.AutoSize = true;
+            this.lblForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForm.Location = new System.Drawing.Point(27, 42);
+            this.lblForm.Name = "lblForm";
+            this.lblForm.Size = new System.Drawing.Size(348, 25);
+            this.lblForm.TabIndex = 1;
+            this.lblForm.Text = "BIENVENIDO AL FORMULARIO 1:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblForm);
+            this.Controls.Add(this.btnEnviarMensaje);
+            this.Name = "Form1";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviarMensaje;
+        private System.Windows.Forms.Label lblForm;
+    }
+}
+
---
+++ Lab01/Lab01/Form2.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Lab01
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFormulario1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFormulario1
+            // 
+            this.btnFormulario1.BackColor = System.Drawing.Color.Green;
+            this.btnFormulario1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFormulario1.Location = new System.Drawing.Point(462, 150);
+            this.btnFormulario1.Name = "btnFormulario1";
+            this.btnFormulario1.Size = new System.Drawing.Size(248, 55);
+            this.btnFormulario1.TabIndex = 0;
+            this.btnFormulario1.Text = "Abrir FORM 1";
+            this.btnFormulario1.UseVisualStyleBackColor = false;
+            this.btnFormulario1.Click += new System.EventHandler(this.btnFormulario1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab01.Properties.Resources.descarga1;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFormulario1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFormulario1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
