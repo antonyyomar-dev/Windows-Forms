499004ac5dd6c852659d91d04f0d346fc8c160ce
Antony Yomar Peña Roña <antonyyomar202403@gmail.com>
2026-09-13 23:58:25 -0500

    Taller de Autos - Windows Forms

---
+++ Lab03/Lab03/entites/Atencion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03.entites
+{
+    public class Atencion
+    {
+        public string TipoVehiculo { get; set; }
+        public string TipoServicio { get; set; }
+        public double Monto { get; set; }
+
+    }
+}
